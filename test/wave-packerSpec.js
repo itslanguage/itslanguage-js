df0a8e75bfc8b8dd00dee5ac8b92e029079bf056
Lehren <loek-ehren@hotmail.com>
2016-10-04 10:40:50 +0200

    test: correct existing tests
    correct expectations and globals

--- test/wave-packerSpec.js
+++ test/wave-packerSpec.js
@@ -1,36 +1,27 @@
-/* eslint-disable
- camelcase,
+/* camelcase,
  new-cap
  */
 
-/* global
- describe,
- expect,
- it,
- spyOn,
- window,
- FormData
- */
 
 const WavePacker = require('../wave-packer');
 
-describe('Warning tests', function() {
+describe('WavePacker warning tests', function() {
   it('Should warn when the sample rates are invalid', function() {
     spyOn(console, 'warn');
     var warning = '48000 or 44100 are the only supported recordingSampleRates';
     var wavePacker = new WavePacker();
     wavePacker.init(0, 0, 0);
     expect(console.warn).toHaveBeenCalledWith(warning);
-    expect(wavePacker.recordingSampleRate).toBeDefined();
-    expect(wavePacker.sampleRate).toBeDefined();
-    expect(wavePacker.channels).toBeDefined();
+    expect(wavePacker.recordingSampleRate).toBe(0);
+    expect(wavePacker.sampleRate).toBe(0);
+    expect(wavePacker.channels).toBe(0);
 
     warning = 'sampleRate must be equal, half or a quarter of the ' +
       'recording sample rate';
     wavePacker.init(48000, 0, 0);
     expect(console.warn).toHaveBeenCalledWith(warning);
-    expect(wavePacker.recordingSampleRate).toBeDefined();
-    expect(wavePacker.sampleRate).toBeDefined();
-    expect(wavePacker.channels).toBeDefined();
+    expect(wavePacker.recordingSampleRate).toBe(48000);
+    expect(wavePacker.sampleRate).toBe(0);
+    expect(wavePacker.channels).toBe(0);
   });
 });
