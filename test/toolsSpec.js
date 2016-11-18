6d22e1fa30b09f15f518b9c49113c8a585776a6d
Lehren <loek-ehren@hotmail.com>
2016-11-18 09:55:17 +0100

    feat(tools): add tickcb checking
    Check if tickCb exists and throw an error if it does not.

--- test/toolsSpec.js
+++ test/toolsSpec.js
@@ -9,32 +9,43 @@ describe('Stopwatch', () => {
     expect(stopwatch.tickCb).toEqual(cb);
   });
 
+  it('should throw an error if tickCb is missing', () => {
+    expect(() => {
+      new Stopwatch();
+    }).toThrowError('tickCb parameter required');
+  });
+
   it('should start ticking', done => {
-    const stopwatch = new Stopwatch();
-    spyOn(stopwatch, 'update');
+    const cb = jasmine.createSpy();
+    const stopwatch = new Stopwatch(cb);
+    spyOn(stopwatch, 'update').and.callThrough();
     stopwatch.start();
     setTimeout(() => {
       expect(stopwatch.update).toHaveBeenCalledTimes(10);
+      expect(cb).toHaveBeenCalledTimes(10);
       done();
     }, 1000);
   });
 
   it('should stop ticking', done => {
-    const stopwatch = new Stopwatch();
-    spyOn(stopwatch, 'update');
+    const cb = jasmine.createSpy();
+    const stopwatch = new Stopwatch(cb);
+    spyOn(stopwatch, 'update').and.callThrough();
     stopwatch.start();
     setTimeout(() => {
       expect(stopwatch.interval).not.toBeNull();
       stopwatch.stop();
       expect(stopwatch.interval).toBeNull();
       expect(stopwatch.update).toHaveBeenCalledTimes(3);
+      expect(cb).toHaveBeenCalledTimes(4);
       done();
     }, 300);
   });
 
   it('should reset count', done => {
     console.log('reset');
-    const stopwatch = new Stopwatch();
+    const cb = jasmine.createSpy();
+    const stopwatch = new Stopwatch(cb);
     stopwatch.start();
     setTimeout(() => {
       stopwatch.stop();
@@ -42,6 +53,7 @@ describe('Stopwatch', () => {
       stopwatch.reset();
       expect(stopwatch.value).toEqual(0);
       console.log('reset done');
+      expect(cb).toHaveBeenCalledTimes(5);
       done();
     }, 300);
   });
@@ -55,13 +67,5 @@ describe('Stopwatch', () => {
     stopwatch.update();
     expect(stopwatch.value).toEqual(2);
     expect(cb).toHaveBeenCalledWith(1);
-  });
-
-  it('should update without cb', () => {
-    const stopwatch = new Stopwatch();
-    stopwatch.update();
-    expect(stopwatch.value).toEqual(1);
-    stopwatch.update();
-    expect(stopwatch.value).toEqual(2);
   });
 });
