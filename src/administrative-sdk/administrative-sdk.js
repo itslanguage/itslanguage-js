922a93c0ff1605ade762a181eaa84e98685f2960
Loek Ehren <loek-ehren@hotmail.com>
2016-12-14 16:06:54 +0100

    docs(adminsdk): add docs to methods

--- src/administrative-sdk/administrative-sdk.js
+++ src/administrative-sdk/administrative-sdk.js
@@ -23,102 +23,335 @@ export default class AdministrativeSDK {
     this._studentController = new StudentController(this._connection);
   }
 
+  /**
+   * Create a choice challenge. The choice challenge will be created in the current active {@link Organisation} derived
+   * from the OAuth2 scope.
+   * It is necessary for a choice challenge to exist for a recording to be valid.
+   *
+   * @param {ChoiceChallenge} choiceChallenge - Object to create.
+   * @returns {Promise} Containing the newly created object.
+   * @throws {Promise} If the server returned an error.
+   */
   createChoiceChallenge(choiceChallenge) {
     return this._choiceChallengeController.createChoiceChallenge(choiceChallenge);
   }
 
+  /**
+   * Get a choice challenge. A choice challenge is identified by its identifier and the current active
+   * {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @param {ChoiceChallenge#id} challengeId - Specify a choice challenge identifier.
+   * @returns {Promise} Containing a ChoiceChallenge.
+   * @throws {Promise} {@link ChoiceChallenge#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getChoiceChallenge(challengeId) {
     return this._choiceChallengeController.getChoiceChallenge(challengeId);
   }
 
+  /**
+   * List all choice challenges in the current active {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @returns {Promise} Containing an array of ChoiceChallenges.
+   * @throws {Promise} If no result could not be found.
+   */
   listChoiceChallenges() {
     return this._choiceChallengeController.listChoiceChallenges();
   }
 
+  /**
+   * Start a choice recognition from streaming audio.
+   *
+   * @param {ChoiceChallenge} challenge - The choice challenge to perform.
+   * @param {AudioRecorder} recorder - The audio recorder to extract audio from.
+   * @param {boolean} [trim=true] - Whether to trim the start and end of recorded audio.
+   * @returns {Promise} A {@link https://github.com/cujojs/when} Promise containing a {@link ChoiceRecognition}.
+   * @emits {string} 'ReadyToReceive' when the call is made to receive audio. The recorder can now send audio.
+   * @throws {Promise} {@link ChoiceChallenge} parameter is required or invalid.
+   * @throws {Promise} {@link ChoiceChallenge#id} field is required.
+   * @throws {Promise} If the connection is not open.
+   * @throws {Promise} If the recorder is already recording.
+   * @throws {Promise} If a recognition session is already in progress.
+   * @throws {Promise} If something went wrong during analysis.
+   */
   startStreamingChoiceRecognition(challenge, recorder, trim) {
     return this._choiceRecognitionController.startStreamingChoiceRecognition(challenge, recorder, trim);
   }
 
+  /**
+   * Get a choice recognition in a choice challenge from the current active {@link Organisation} derived from
+   * the OAuth2 scope.
+   *
+   * @param {ChoiceChallenge#id} challengeId - Specify a choice challenge identifier.
+   * @param {ChoiceRecognition#id} recognitionId - Specify a choice recognition identifier.
+   * @returns {Promise} Promise containing a ChoiceRecognition.
+   * @throws {Promise} {@link ChoiceChallenge#id} field is required.
+   * @throws {Promise} {@link ChoiceRecognition#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getChoiceRecognition(challengeId, recognitionId) {
     return this._choiceRecognitionController.getChoiceRecognition(challengeId, recognitionId);
   }
 
+  /**
+   * List all choice recognitions in a specific {@link ChoiceChallenge} from the current active {@link Organisation}
+   * derived from the OAuth2 scope.
+   *
+   * @param {ChoiceChallenge#id} challengeId - Specify a choice challenge to list speech recognitions for.
+   * @returns {Promise} Promise containing an array of ChoiceRecognitions.
+   * @throws {Promise} {@link ChoiceChallenge#id} is required.
+   * @throws {Promise} If no result could not be found.
+   */
   listChoiceRecognitions(challengeId) {
     return this._choiceRecognitionController.listChoiceRecognitions(challengeId);
   }
 
+  /**
+   * Create an organisation. The organisation will be owned by the current active tenant.
+   *
+   * @param {Organisation} organisation - Object to create.
+   * @returns {Promise} Promise containing the newly created object.
+   * @throws {Promise} If the server returned an error.
+   */
   createOrganisation(organisation) {
     return this._organisationController.createOrganisation(organisation);
   }
 
+  /**
+   * Get an organisation. You can only get an organisation the current tenant is an owner of.
+   *
+   * @param {Organisation#id} organisationId - Specify an organisation identifier.
+   * @returns {Promise} Promise containing an Organisation.
+   * @throws {Promise} {@link Organisation#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getOrganisation(organisationId) {
     return this._organisationController.getOrganisation(organisationId);
   }
 
+  /**
+   * List all organisations the current tenant is owner of.
+   *
+   * @returns {Promise} Promise containing a list of Organisations.
+   * @throws {Promise} If no result could not be found.
+   */
   listOrganisations() {
     return this._organisationController.listOrganisations();
   }
 
+  /**
+   * Start a pronunciation analysis from streaming audio.
+   *
+   * @param {PronunciationChallenge} challenge - The pronunciation challenge to perform.
+   * @param {AudioRecorder} recorder - The audio recorder to extract audio from.
+   * @param {boolean} [trim] - Whether to trim the start and end of recorded audio (default: true).
+   * @returns {Promise} A {@link https://github.com/cujojs/when} Promise containing a {@link PronunciationAnalysis}.
+   * @emits {string} 'ReadyToReceive' when the call is made to receive audio. The recorder can now send audio.
+   * @emits {Object} When the sent audio has finished alignment. Aligning audio is the process of mapping the audio
+   * to spoken words and determining when what is said. An object is sent containing a property 'progress',
+   * which is the sent audio alignment, and a property 'referenceAlignment' which is the alignment of the
+   * reference audio.
+   * @throws {Promise} If challenge is not an object or not defined.
+   * @throws {Promise} If challenge has no id.
+   * @throws {Promise} If the connection is not open.
+   * @throws {Promise} If the recorder is already recording.
+   * @throws {Promise} If a session is already in progress.
+   * @throws {Promise} If something went wrong during analysis.
+   */
   startStreamingPronunciationAnalysis(challenge, recorder, trim) {
     return this._pronAnalaController.startStreamingPronunciationAnalysis(challenge, recorder, trim);
   }
 
+  /**
+   * Get a pronunciation analysis in a pronunciation challenge from the current active {@link Organisation} derived
+   * from the OAuth2 scope.
+   *
+   * @param {PronunciationChallenge#id} challengeId - Specify a pronunciation challenge identifier.
+   * @param {PronunciationAnalysis#id} analysisId - Specify a pronunciation analysis identifier.
+   * @returns {Promise} Promise containing a PronunciationAnalysis.
+   * @throws {Promise} {@link PronunciationChallenge#id} field is required.
+   * @throws {Promise} {@link PronunciationAnalysis#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getPronunciationAnalysis(challengeId, analysisId) {
     return this._pronAnalaController.getPronunciationAnalysis(challengeId, analysisId);
   }
 
+  /**
+   * List all pronunciation analyses in a specific pronunciation challenge from the current active {@link Organisation}
+   * derived from the OAuth2 scope.
+   *
+   * @param {PronunciationChallenge#id} challengeId - Specify a pronunciation challenge identifier to list
+   * speech recordings for.
+   * @param {boolean} [detailed=false] - Returns extra analysis metadata when true.
+   * @returns {Promise} Promise containing a list of PronunciationAnalyses.
+   * @throws {Promise} {@link PronunciationChallenge#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   listPronunciationAnalyses(challengeId, detailed) {
     return this._pronAnalaController.listPronunciationAnalyses(challengeId, detailed);
   }
 
+  /**
+   * Create a pronunciation challenge. The created challenge will be part of the current active {@link Organisation}
+   * derived from the OAuth2 scope.
+   *
+   * @param {PronunciationChallenge} challenge - Object to create..
+   * @returns {Promise} Promise containing the newly created object.
+   * @throws {Promise} {@link PronunciationChallenge#referenceAudio} of type "Blob" is required.
+   * @throws {Promise} If the server returned an error.
+   */
   createPronunciationChallenge(challenge) {
     return this._pronChallController.createPronunciationChallenge(challenge);
   }
 
+  /**
+   * Get a pronunciation challenge from the current active {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @param {PronunciationChallenge#id} challengeId - Specify a pronunciation challenge identifier.
+   * @returns {Promise} Promise containing a PronunciationChallenge.
+   * @throws {Promise} {@link PronunciationChallenge#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getPronunciationChallenge(challengeId) {
     return this._pronChallController.getPronunciationChallenge(challengeId);
   }
 
+  /**
+   * List all pronunciation challenges in the current active {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @returns {Promise} Promise containing a list of PronunciationChallenges.
+   * @throws {Promise} If no result could not be found.
+   */
   listPronunciationChallenges() {
     return this._pronChallController.listPronunciationChallenges();
   }
 
+  /**
+   * Delete a pronunciation challenge from the current active {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @param {PronunciationChallenge#id} challengeId - A pronunciation challenge identifier.
+   * @returns {Promise} Promise containing this.
+   * @throws {Promise} {@link PronunciationChallenge#id} field is required.
+   * @throws {Promise} If the server returned an error.
+   */
   deletePronunciationChallenge(challengeId) {
     return this._pronChallController.deletePronunciationChallenge(challengeId);
   }
 
+  /**
+   * Create a speech challenge in the current active {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @param {SpeechChallenge} speechChallenge - Object to create.
+   * @returns {Promise} Promise containing the newly created object.
+   * @throws {Promise} If the server returned an error.
+   */
   createSpeechChallenge(speechChallenge) {
     return this._speechChallengeController.createSpeechChallenge(speechChallenge);
   }
 
+  /**
+   * Get a speech challenge from the current active {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @param {SpeechChallenge#id} challengeId - Specify a speech challenge identifier.
+   * @returns {Promise} Promise containing a SpeechChallenge.
+   * @throws {Promise} {@link SpeechChallenge#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getSpeechChallenge(challengeId) {
     return this._speechChallengeController.getSpeechChallenge(challengeId);
   }
 
+  /**
+   * List all speech challenges in the current active {@link Organisation} derived from the OAuth2 scope.
+   *
+   * @returns {Promise} Promise containing a list of SpeechChallenges.
+   * @throws {Promise} If no result could not be found.
+   */
   listSpeechChallenges() {
     return this._speechChallengeController.listSpeechChallenges();
   }
 
+  /**
+   * Start a speech recording from streaming audio.
+   *
+   * @param {SpeechChallenge} challenge - The speech challenge to perform.
+   * @param {AudioRecorder} recorder - The audio recorder to extract audio from.
+   * @returns {Promise} A {@link https://github.com/cujojs/when} Promise containing a {@link SpeechRecording}.
+   * @emits {string} 'ReadyToReceive' when the call is made to receive audio. The recorder can now send audio.
+   * @throws {Promise} If challenge is not an object or not defined.
+   * @throws {Promise} If challenge has no id.
+   * @throws {Promise} If the connection is not open.
+   * @throws {Promise} If the recorder is already recording.
+   * @throws {Promise} If a session is already in progress.
+   * @throws {Promise} If something went wrong during recording.
+   */
   startStreamingSpeechRecording(challenge, recorder) {
     return this._speechRecordingController.startStreamingSpeechRecording(challenge, recorder);
   }
 
+  /**
+   * Get a speech recording in a speech challenge from the current active {@link Organisation} derived from the OAuth2
+   * scope.
+   *
+   * @param {SpeechChallenge#id} challengeId - Specify a speech challenge identifier.
+   * @param {SpeechRecording#id} recordingId - Specify a speech recording identifier.
+   * @returns {Promise} Promise containing a SpeechRecording.
+   * @throws {Promise} {@link SpeechChallenge#id} field is required.
+   * @throws {Promise} {@link SpeechRecording#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getSpeechRecording(challengeId, recordingId) {
     return this._speechRecordingController.getSpeechRecording(challengeId, recordingId);
   }
 
+  /**
+   * List all speech recordings in a specific speech challenge from the current active {@link Organisation} derived
+   * from the OAuth2 scope.
+   *
+   * @param {SpeechChallenge#id} challengeId - Specify a speech challenge identifier to list speech recordings for.
+   * @returns {Promise} Promise containing a list of SpeechRecording.
+   * @throws {Promise} {@link SpeechChallenge#id} is required.
+   * @throws {Promise} If no result could not be found.
+   */
   listSpeechRecordings(challengeId) {
     return this._speechRecordingController.listSpeechRecordings(challengeId);
   }
 
+  /**
+   * Create a student.
+   *
+   * @param {Student} student - Object to create.
+   * @returns {Promise} Promise containing the newly created object.
+   * @throws {Promise} {@link Student#organisationId} field is required.
+   * @throws {Promise} If the server returned an error.
+   */
   createStudent(student) {
     return this._studentController.createStudent(student);
   }
 
+  /**
+   * Get a student.
+   *
+   * @param {Organisation#id} organisationId - Specify an organisation identifier.
+   * @param {Student#id} studentId - Specify a student identifier.
+   * @returns {Promise} Promise containing a Student.
+   * @throws {Promise} {@link Organisation#id} field is required.
+   * @throws {Promise} {@link Student#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   getStudent(studentId) {
     return this._studentController.getStudent(studentId);
   }
 
+  /**
+   * List all students in the organisation.
+   *
+   * @param {Organisation#id} organisationId - Specify an organisation identifier.
+   * @returns {Promise} Promise containing a list of Students.
+   * @throws {Promise} {@link Organisation#id} field is required.
+   * @throws {Promise} If no result could not be found.
+   */
   listStudents() {
     return this._studentController.listStudents();
   }
