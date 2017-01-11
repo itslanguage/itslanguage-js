4d3acfb14c054cde866905309acc478be5ebc6d0
Loek Ehren <loek-ehren@hotmail.com>
2017-01-11 11:02:14 +0100

    fix(profile): fix error message
    Fix error message to show correct type.

--- src/administrative-sdk/profile/profile.js
+++ src/administrative-sdk/profile/profile.js
@@ -56,7 +56,7 @@ export default class Profile {
     this.gender = gender;
 
     if (!(birthDate instanceof Date)) {
-      throw new Error('birthDate parameter of type "string" is required');
+      throw new Error('birthDate parameter of type "Date" is required');
     }
 
     /**
