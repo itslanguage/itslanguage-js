1506bebc9da190f5be19886346062d1bc21f1c7c
Loek Ehren <loek-ehren@hotmail.com>
2017-01-12 15:21:17 +0100

    refactor(role): check params first
    Check params first before setting. Change error message.

--- src/administrative-sdk/role/role.js
+++ src/administrative-sdk/role/role.js
@@ -8,21 +8,21 @@ export default class Role {
    * @param {string} name - Name of the role.
    * @param {Array.<string>} permissions - Permissions this role can take on.
    * @throws {Error} role parameter of type "string" is required.
-   * @throws {Error} permission parameter of type "Array" is required.
+   * @throws {Error} permission parameter of type "Array.<string>" is required.
    */
   constructor(name, permissions) {
     if (typeof name !== 'string') {
       throw new Error('role parameter of type "string" is required');
     }
 
+    if (!(permissions instanceof Array) || permissions.length === 0) {
+      throw new Error('permission parameter of type "Array.<string>" is required');
+    }
+
     /**
      * @type {string} Name of the role.
      */
     this.name = name;
-
-    if (!(permissions instanceof Array) || permissions.length === 0) {
-      throw new Error('permission parameter of type "Array" is required');
-    }
 
     /**
      * @type {Array.<string>} Permissions this role can take on.
