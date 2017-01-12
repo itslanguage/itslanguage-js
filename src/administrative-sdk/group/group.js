b3f32a4656a6aacb1f041e5f86672a03ad0485aa
Loek Ehren <loek-ehren@hotmail.com>
2017-01-12 13:42:16 +0100

    docs(group): make id optional in docs

--- src/administrative-sdk/group/group.js
+++ src/administrative-sdk/group/group.js
@@ -5,7 +5,7 @@ export default class Group {
   /**
    * Create a group.
    *
-   * @param {string} [id] - The group identifier. If none is given, one is generated.
+   * @param {?string} id - The group identifier. If none is given, one is generated.
    * @param {string} name - The name of the group.
    * @throws {Error} id parameter of type "string|null" is required
    * @throws {Error} name parameter of type "string" is required
