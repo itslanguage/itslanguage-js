c0e72ac530b408b5e36b3da6104f5c3960f54d2d
Loek Ehren <loek-ehren@hotmail.com>
2017-01-12 13:50:07 +0100

    refactor(profile): check all params first before setting

--- src/administrative-sdk/profile/profile.js
+++ src/administrative-sdk/profile/profile.js
@@ -20,15 +20,27 @@ export default class Profile {
       throw new Error('firstName parameter of type "string" is required');
     }
 
+    if (typeof lastName !== 'string') {
+      throw new Error('lastName parameter of type "string" is required');
+    }
+
+    if (infix !== null && typeof infix !== 'string') {
+      throw new Error('infix parameter of type "string|null" is required');
+    }
+
+    if (typeof gender !== 'string') {
+      throw new Error('gender parameter of type "string" is required');
+    }
+
+    if (!(birthDate instanceof Date)) {
+      throw new Error('birthDate parameter of type "Date" is required');
+    }
+
     /**
      * The first name of the {@link User}.
      * @type {string}
      */
     this.firstName = firstName;
-
-    if (typeof lastName !== 'string') {
-      throw new Error('lastName parameter of type "string" is required');
-    }
 
     /**
      * The last name of the {@link User}.
@@ -36,28 +48,16 @@ export default class Profile {
      */
     this.lastName = lastName;
 
-    if (infix !== null && typeof infix !== 'string') {
-      throw new Error('infix parameter of type "string|null" is required');
-    }
-
     /**
      * @type {string} The infix of the {@link User}'s name.
      */
     this.infix = infix;
-
-    if (typeof gender !== 'string') {
-      throw new Error('gender parameter of type "string" is required');
-    }
 
     /**
      * The gender of the {@link User}.
      * @type {string}
      */
     this.gender = gender;
-
-    if (!(birthDate instanceof Date)) {
-      throw new Error('birthDate parameter of type "Date" is required');
-    }
 
     /**
      * The birth date of the {@link User}.
