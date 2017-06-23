446e684aa50b2c2ec97df23b608bef41b0952bde
Mathijs van den Worm <m.a.vandenworm@gmail.com>
2017-06-23 12:26:04 +0200

    refactor: untangle websockets
    Trim down the floof around the websocket features by exposing them as
    functions instead of methods.
    
    Also document them more verbosely both inline and in the JSdocs.

--- src/api/communication.spec.js
+++ src/api/communication.spec.js
@@ -4,6 +4,7 @@
  */
 
 import * as communication from './communication';
+import autobahn from 'autobahn';
 
 const TEST_API_URL = 'https://www.example.com';
 
@@ -24,6 +25,219 @@ describe('updateSettings', () => {
 
     expect(() => communication.updateSettings(faultyNewSettings))
       .toThrowError(Error, 'Please, only provide objects as settings.');
+  });
+});
+
+
+describe('handleWebsocketAuthorisationChallenge', () => {
+  /* Let it be known that this is a extremely hacky way to test internal code.
+   *
+   * The authorisation mechanism of authobahn might be triggered and challenge
+   * the given token. We do have a internal function as this is of no use
+   * anywhere else in the SDK or beyond.
+   *
+   * The `onchallenge` handler provided in the `autobahn.Connection`
+   * constructor options parameter can be triggered through the instance of the
+   * connection using its "internal/private" _options property.
+   */
+  let onchallenge;
+
+  beforeEach(done => {
+    spyOn(autobahn.Connection.prototype, 'close');
+    const connectionOpenSpy = spyOn(autobahn.Connection.prototype, 'open');
+    connectionOpenSpy.and.callFake(function() {
+      // Get a reference to the thing we actually want to test.
+      onchallenge = this._options.onchallenge;
+      this.onopen();
+    });
+    communication.openWebsocketConnection()
+      .then(done);
+  });
+
+  afterEach(done => {
+    // Make sure there is no existing connection after each test.
+    communication.closeWebsocketConnection()
+      .then(done);
+  });
+
+  it('should return the `authorizationToken` when the ticket method is used', () => {
+    communication.updateSettings({authorizationToken: 'much_secure'});
+    const session = {};
+    expect(onchallenge(session, 'ticket')).toEqual('much_secure');
+  });
+
+  it('should throw a Error when a unsupported method is used', () => {
+    const session = {};
+    const errorMessage = 'The websocket server tried to use the unknown ' +
+                         'authentication challenge: "unsupported method"';
+    expect(() => onchallenge(session, 'unsupported method')).toThrowError(Error, errorMessage);
+  });
+});
+
+
+describe('openWebsocketConnection', () => {
+  afterEach(done => {
+    // Make sure there is no existing connection after each test.
+    communication.closeWebsocketConnection()
+      .then(done);
+  });
+
+  it('should open a connection if there isn\'t one already', done => {
+    const connectionOpenSpy = spyOn(autobahn.Connection.prototype, 'open');
+    connectionOpenSpy.and.callFake(function() {
+      this.onopen();
+    });
+
+    communication.openWebsocketConnection()
+      .then(result => {
+        expect(result).toEqual('Successfully established a websocket connection.');
+        done();
+      }, fail);
+  });
+
+  it('should reject if the connection could not be established', done => {
+    communication.openWebsocketConnection()
+      .then(fail, result => {
+        expect(result).toEqual('The connection is erroneous; check if all ' +
+                               'required settings have been injected using ' +
+                               'the `updateSettings()` function. If the ' +
+                               'problem persists please post a issue on our ' +
+                               'GitHub repository.');
+        // There shouldn't be a reference to the erroneos connection. We can
+        // validate this by trying to "close" the connection. The
+        // `closeWebsocketConnection` should not pass its falsy check and thus
+        // resolve in 'There is no websocket connection to close.'.
+        return communication.closeWebsocketConnection();
+      })
+      .then(result => {
+        expect(result).toEqual('There is no websocket connection to close.');
+        done();
+      }, fail);
+  });
+});
+
+
+describe('closeWebsocketConnection', () => {
+  let connectionOpenSpy;
+  let connectionCloseSpy;
+
+  beforeEach(() => {
+    connectionCloseSpy = spyOn(autobahn.Connection.prototype, 'close');
+    connectionOpenSpy = spyOn(autobahn.Connection.prototype, 'open');
+    connectionOpenSpy.and.callFake(function() {
+      this.onopen();
+    });
+  });
+
+  afterEach(done => {
+    // Make sure there is no existing connection after each test.
+    communication.closeWebsocketConnection()
+      .then(done);
+  });
+
+  it('should resolve if there is no open connection', done => {
+    communication.closeWebsocketConnection()
+      .then(result => {
+        expect(result).toEqual('There is no websocket connection to close.');
+        done();
+      }, fail);
+  });
+
+  it('should successfully close a open connection', done => {
+    communication.openWebsocketConnection()
+      // Now we've opened the connection; close it again.
+      .then(() => communication.closeWebsocketConnection(), fail)
+      .then(result => {
+        expect(result).toEqual('The websocket connection has been closed successfully.');
+        done();
+      }, fail);
+  });
+
+  it('should resolve when the connection was already closed', done => {
+    connectionCloseSpy.and.callFake(() => {
+      throw new Error('connection already closed');
+    });
+
+    communication.openWebsocketConnection()
+      // Now we've opened the connection; close it again.
+      .then(() => communication.closeWebsocketConnection(), fail)
+      .then(result => {
+        expect(result).toEqual('The websocket connection has already been closed.');
+        done();
+      }, fail);
+  });
+
+  it('should also remove the reference to the connection once it is closed', done => {
+    communication.openWebsocketConnection()
+      // Now we've opened the connection; close it again.
+      .then(() => communication.closeWebsocketConnection(), fail)
+      .then(() => communication.closeWebsocketConnection(), fail)
+      .then(result => {
+        // The internal reference is set to `null` when the connection is
+        // closed. The `closeWebsocketConnection` function would therefore not
+        // pass its falsy check. The result should therefore be the same as:
+        // `it('should resolve if there is no open connection', ...);`
+        expect(result).toEqual('There is no websocket connection to close.');
+        done();
+      }, fail);
+  });
+});
+
+
+describe('makeWebsocketCall', () => {
+  let connectionOpenSpy;
+  let connectionSessionStub;
+
+  beforeEach(() => {
+    spyOn(autobahn.Connection.prototype, 'close');
+    connectionSessionStub = jasmine.createSpyObj('Session', ['call']);
+    connectionOpenSpy = spyOn(autobahn.Connection.prototype, 'open');
+    connectionOpenSpy.and.callFake(function() {
+      // This property is returned through the session "property" of a
+      // conncection instance. Sadly only the get is defined with the
+      // `Object.defineProperty` which forces us to mock the internals.
+      this._session = connectionSessionStub;
+      this.onopen();
+    });
+  });
+
+  afterEach(done => {
+    // Make sure there is no existing connection after each test.
+    communication.closeWebsocketConnection()
+      .then(done);
+  });
+
+  it('should prefix the `rpc` parameter and pass the rest into the websocket session call', done => {
+    communication.openWebsocketConnection()
+      .then(() => communication.makeWebsocketCall(
+        'do.a.rpc',
+        ['accept', 'these'],
+        {kwarg: 'value'},
+        {option: 'value'}
+      ))
+      .then(() => {
+        expect(connectionSessionStub.call).toHaveBeenCalledWith(
+          'nl.itslanguage.do.a.rpc',
+          ['accept', 'these'],
+          {kwarg: 'value'},
+          {option: 'value'}
+        );
+        done();
+      }, fail);
+  });
+
+  it('should open a websocket connection if there isn\'t one already', done => {
+    communication.makeWebsocketCall('do.a.rpc', ['accept', 'these'], {kwarg: 'value'}, {option: 'value'})
+      .then(() => {
+        expect(connectionOpenSpy).toHaveBeenCalled();
+        expect(connectionSessionStub.call).toHaveBeenCalledWith(
+          'nl.itslanguage.do.a.rpc',
+          ['accept', 'these'],
+          {kwarg: 'value'},
+          {option: 'value'}
+        );
+        done();
+      }, fail);
   });
 });
 
