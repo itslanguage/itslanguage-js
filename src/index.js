10a5bc43273311bd576f5a24962fff3d34be812c
Jogchum Koerts <jogchum.koerts@itslanguage.nl>
2018-08-07 23:45:08 +0200

    chore: remove the old class based sdk files

--- src/index.js
+++ src/index.js
@@ -1,47 +1,14 @@
 import {default as VolumeMeter, generateWaveSample} from './audio/audio-tools';
-import AdministrativeSDK from './administrative-sdk/administrative-sdk';
 import AudioPlayer from './audio/audio-player';
 import AudioRecorder from './audio/audio-recorder';
-import BasicAuth from './administrative-sdk/basic-auth/basic-auth';
-import Category from './administrative-sdk/category/category';
-import ChoiceChallenge from './administrative-sdk/choice-challenge/choice-challenge';
-import Connection from './administrative-sdk/connection/connection-controller';
-import EmailCredentials from './administrative-sdk/email-credentials/email-credentials';
-import Group from './administrative-sdk/group/group';
-import Organisation from './administrative-sdk/organisation/organisation';
 import Player from './WebAudio/Player';
-import Profile from './administrative-sdk/profile/profile';
-import Progress from './administrative-sdk/progress/progress';
-import PronunciationChallenge from './administrative-sdk/pronunciation-challenge/pronunciation-challenge';
-import Role from './administrative-sdk/role/role';
-import SpeechChallenge from './administrative-sdk/speech-challenge/speech-challenge';
 import Stopwatch from './audio/tools';
-import User from './administrative-sdk/user/user';
 
 export {
-  AdministrativeSDK,
-  BasicAuth,
-  Category,
-  ChoiceChallenge,
-  Connection,
-  EmailCredentials,
-  Group,
-  Organisation,
-  Profile,
-  Progress,
-  PronunciationChallenge,
-  Role,
-  SpeechChallenge,
-  User,
-
   AudioPlayer,
   AudioRecorder,
-
   generateWaveSample,
-
   Stopwatch,
-
   VolumeMeter,
-
   Player
 };
