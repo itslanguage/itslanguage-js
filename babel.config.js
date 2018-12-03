3b71ba03ff154c17109ce5177d833f971547d074
Jogchum Koerts <jogchum.koerts@itslanguage.nl>
2018-12-03 14:08:56 +0100

    chore(babel.config.js): converted .babelrc file

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env', {
+        targets: {
+          esmodules: true,
+        },
+        modules: false,
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+  env: {
+    test: {
+      presets: [
+        '@babel/preset-env',
+      ],
+      plugins: [
+        [
+          'istanbul', {
+            exclude: [
+              'test/**',
+              '**/*.spec.js',
+            ],
+          },
+        ],
+      ],
+    },
+  },
+};
