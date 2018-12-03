6c4888734705e488afe6807aa95dfab4968c2ae2
Jogchum Koerts <jogchum.koerts@itslanguage.nl>
2018-12-03 14:27:34 +0100

    chore(babel): use loose for class props plugin

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,9 @@ module.exports = {
     ],
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-class-properties', {
+      loose: true,
+    }],
     ['@babel/plugin-proposal-object-rest-spread', {
       loose: true,
       useBuiltIns: true,
