fd82a6190b76d3cf07cb6c2abe05e8829468bc48
Jogchum Koerts <jogchum.koerts@itslanguage.nl>
2018-12-03 16:21:11 +0100

    refactor(build): introduce packer/build to build
    - Removed scripts/build.js
    - Update package.json
      - Call scrips/packer/build.js for the build script.
      - Remove pre and post build step (not needed anymore).
      - Remove jsdoc step, build process takes care of this.
      - Altered the clean:all step (removed jsdoc).
    
    The scripts/packer/build.js file will build packages defined in
    bundle.js file. It is heavily inspired on the React process with rollup,
    event though we use webpack (autobahn party all-the-way).

---
+++ scripts/packer/bundles.js
@@ -0,0 +1,32 @@
+// Files that should be available in each package
+const defaultFiles = [
+  'package.json',
+  'README.md',
+  'CHANGELOG.md',
+  'LICENSE',
+];
+
+/**
+ * Specify some bundle information.
+ *
+ * Entry: module id of the package. Is also used as path where the pacakge lives.
+ * Library: this id will be used to generate the UMD package with.
+ * files: array with files to always copy.
+ *
+ * @type {{
+ *   entry: string,
+ *   library: string,
+ *   files: string[]
+ * }[]}
+ */
+const bundles = [
+  {
+    entry: 'api',
+    library: 'itsl.api',
+    files: [
+      ...defaultFiles,
+    ],
+  },
+];
+
+module.exports = bundles;
