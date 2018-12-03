dbfc332b870ddba7307b182b243849ab26349597
Jogchum Koerts <jogchum.koerts@itslanguage.nl>
2018-12-03 14:23:57 +0100

    chore(babel): add object spread plugin
    Add the plugin and use it this time. Also set it to use builtins (i.e.
    use Object.Assign) in stead of yet-another-dep.

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,10 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-object-rest-spread', {
+      loose: true,
+      useBuiltIns: true,
+    }],
   ],
   env: {
     test: {
