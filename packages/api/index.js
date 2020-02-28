8483de075545d74f6725c1990def6d362f1c76e0
Jogchum Koerts <jogchum.koerts@itslanguage.nl>
2020-02-28 21:03:56 +0100

    buid: prepare new patch release

--- packages/api/index.js
+++ packages/api/index.js
@@ -22,7 +22,7 @@ import * as users from './users';
  * Document the version number of the ITSLanguage API package.
  * @type {string}
  */
-export const version = '5.5.0';
+export const version = '5.5.1';
 
 /**
  * Represents the ITSLanguage API package.
